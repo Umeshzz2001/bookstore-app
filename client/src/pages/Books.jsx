b00770a69de8f012c2263305bdeb2a686aeff24d
Sandeepa Hiripitiya <umeshsandeepa1@gmail.com>
2025-08-05 00:01:29 +0530

    set up the frontend1

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Books = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const res = await axios.get("http://localhost:3000/books");
+        setBooks(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllBooks();
+  }, []);
+
   return (
     <div>
+      <h1>Books store by Umesh</h1>
+      <div className="books">
+        {books.map((book) => (
+          <div className="book" key={book.id}>
+            {book.cover && <img src={book.cover} alt={book.title} />}
+            <h2>{book.title}</h2>
+            <p>{book.description}</p>
+            <span>{book.price}</span>
+          </div>
+        ))}
+      </div>
       
+        <Link to ="/add">
+        <button>
+        Add New Book
+      </button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Books
+export default Books;
